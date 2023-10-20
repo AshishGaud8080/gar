84a2eb592d20c5e52af6f2463ab17d397a121bf3
AshishGaud8080 <ashishmgaud8080>
2023-10-20 20:20:11 +0530

    run

---
+++ ad1hw.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(void)
+{
+	int n1,n2,add;
+	cout<<"enter the n1";
+	cin>>n1;
+	
+	cout<<"enter the n2";
+	cin>>n2;
+	
+	add=n1+n2;
+	cout<<n1<<"+"<<n2<<"="<<add;
+	return 0;
+	
+	
+}
